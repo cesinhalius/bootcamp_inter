ec2ccf934b4242ed295a8e35b94f3a1c0316f0c5
Cesar Augusto <cesinhalius@hotmail.com>
2022-11-03 19:41:40 -0300

    Refatorando os código e separando responsabilidade

--- ListagemPokemon/assets/js/poke_api.js
+++ ListagemPokemon/assets/js/poke_api.js
@@ -1,10 +1,36 @@
 
 const PokeApi ={}
 
+function convertPokeApiDetailToPokemon(pokemonDetail){
+    
+    const pokemon = new Pokemon()
+    pokemon.number = pokemonDetail.order
+    pokemon.name = pokemonDetail.name
+    const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types 
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+
+PokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+          .then((response) => response.json())
+          .then(convertPokeApiDetailToPokemon)
+}
+
 PokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    return fetch(URL)
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(url)
     .then((resp) => resp.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error))
+    .then((pokemonList) => pokemonList.map(PokeApi.getPokemonsDetail))
+    .then((detailRequest) => Promise.all(detailRequest))
+    .then((pokemonDetail) => pokemonDetail)
+
+
 }
