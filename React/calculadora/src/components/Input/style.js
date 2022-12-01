f5cc287d185314407428228b6164cc0fd0c21387
Cesar Augusto <cesinhalius@hotmail.com>
2022-12-01 20:40:12 -0300

    Adicionando a caixa de numeros e adicionando styled-components

---
+++ React/calculadora/src/components/Input/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #aaaaff;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+
+  input {
+    align-items: flex-end;
+    background-color: #aaaaff;
+    color:#FFF;
+    flex-direction:column;
+    display: flex;
+    font-size:24px;
+    height: 75px;
+    padding:0 10px;
+    width: 100%;
+  }
+`;
