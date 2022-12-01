5ffc331126b65ab5739ae6ca569a625eb1c0a2b5
Cesar Augusto <cesinhalius@hotmail.com>
2022-12-01 20:00:03 -0300

    Adicionando estilo para página principal

---
+++ React/calculadora/src/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width:100%;
+    height:100vh;
+    background-color: #CACACA;
+
+    display: flex;
+    align-items:center;
+    justify-content:center;
+`;
+
+
+export const Content = styled.div`
+    background-color:#FFF;
+    width:50%;
+`
+
+export const Row = styled.div`
+    align-items:center
+    display: flex;
+    flex-direction:row;
+    justify-content:space-beetween;
+`
+export const Column = styled.div`
+    align-items:center
+    display: flex;
+    flex-direction:row;
+    justify-content:space-beetween;
+`
