e631bc5accc43499ce87e4c871f5abe3ba0186c4
Cesar Augusto <cesinhalius@hotmail.com>
2022-12-06 17:05:51 -0300

    Primeiro commit do projeto

---
+++ React/wiki-github/src/pages/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      Ola
+    </div>
+  );
+}
+
+export default App;
---
+++ React/wiki-github/src/style.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export default createGlobalStyle`
+
+    *, body{
+        margin: 0;
+        padding:0;
+    }
+
+    body{
+        background-color: #0d1117;
+        color:#fff;
+    }
+
+
+
+`;
