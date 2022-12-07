acf5dc50a796429ec401f34814143ac6b60d4b15
Cesar Augusto <cesinhalius@hotmail.com>
2022-12-06 22:40:16 -0300

    Adicionando estilos no projeto

--- React/wiki-github/src/index.js
+++ React/wiki-github/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './pages/App';
+
+
 import GlobalStyle from './style';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
