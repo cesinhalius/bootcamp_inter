df648f1d47e7369a4b7dfdaf02da2358905a0918
Cesar Augusto <cesinhalius@hotmail.com>
2022-12-07 18:35:15 -0300

    Adicionando as importações dos arquivos

--- React/tela_login_dio/src/App.js
+++ React/tela_login_dio/src/App.js
@@ -1,10 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
+import { Home } from "./pages/home";
+import { Feed } from "./pages/feed";
+import { Login } from "./pages/login";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <Router>
+      <GlobalStyle/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/feed" element={<Feed />}/>
+      </Routes>
+    </Router>
   );
 }
 
